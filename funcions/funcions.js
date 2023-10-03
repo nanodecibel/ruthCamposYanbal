70d0ffe8e6230ecd6fe548acdef65bbe30e29acd
nanodecibel <jallauca@udlanet.ec>
2023-10-03 11:39:15 -0500

    primer commit

--- funcions/funcions.js
+++ funcions/funcions.js
@@ -1,3 +1,5 @@
+
+
 function openLightbox(image) {
     const lightbox = document.querySelector('.lightbox');
     const lightboxImg = lightbox.querySelector('img');
@@ -9,3 +11,42 @@ function closeLightbox() {
     const lightbox = document.querySelector('.lightbox');
     lightbox.style.display = 'none';
 }
+function showInfoAndScroll(divNumber) {
+    // Ocultar todos los divs con id que empiece con "infoDiv"
+    const allInfoDivs = document.querySelectorAll('[id^="infoDiv"]');
+    allInfoDivs.forEach(div => {
+        div.style.display = 'none';
+    });
+
+    // Mostrar el div específico
+    const infoDiv = document.getElementById(`infoDiv${divNumber}`);
+    infoDiv.style.display = 'block';
+
+    // Hacer scroll hacia el div
+    infoDiv.scrollIntoView({ behavior: 'smooth' });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Tu código JavaScript aquí
+    
+    // Escuchar el evento de cambio de pestaña "¿Por qué adquirir un kit de bienvenida?"
+    const tabKitDeBienvenida = document.getElementById('kitDeBienvenida');
+    tabKitDeBienvenida.addEventListener('click', () => {
+        // Ocultar todos los divs con id que empiece con "infoDiv"
+        const allInfoDivs = document.querySelectorAll('[id^="infoDiv"]');
+        allInfoDivs.forEach(div => {
+            div.style.display = 'none';
+        });
+    });
+
+    // Escuchar el evento de cambio de pestaña "Opciones kits de bienvenida"
+    const tabElInicioPerfecto = document.getElementById('elInicioPerfecto');
+    tabElInicioPerfecto.addEventListener('click', () => {
+        // Ocultar todos los divs con id que empiece con "infoDiv"
+        const allInfoDivs = document.querySelectorAll('[id^="infoDiv"]');
+        allInfoDivs.forEach(div => {
+            div.style.display = 'none';
+        });
+    });
+});
+
